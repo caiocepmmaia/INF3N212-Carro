785c4b8c78367418735463a0121b08b8b5319a68
Caio Eduardo Pinto Maia <181810115@senacinformatica.br>
2023-03-29 21:47:44 -0300

    Desenvolvimento de menu's

--- src/inf3n212/carro/INF3N212Carro.java
+++ src/inf3n212/carro/INF3N212Carro.java
@@ -6,15 +6,18 @@ package inf3n212.carro;
 
 import controller.CCarro;
 import controller.CPessoa;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
  * @author 181810115
  */
 public class INF3N212Carro {
-    
+
     public static CPessoa cadPessoa = new CPessoa();
     public static CCarro cadCarro = new CCarro();
+    static Scanner leia = new Scanner(System.in);
 
     /**
      * @param args the command line arguments
@@ -23,13 +26,49 @@ public class INF3N212Carro {
         // TODO code application logic here
         cadPessoa.mockPessoas();
         cadCarro.mockCarros();
-        
+
         System.out.println(cadPessoa.getPessoas());
         System.out.println("-----");
         System.out.println(cadCarro.getCarros());
-        
-    }
-    
-}
+        System.out.println("Qual?");
 
-    
+    }// fim public main
+
+    public static int leiaNumInt() {
+        Scanner leiaNum = new Scanner(System.in);
+        try {
+            return leiaNum.nextInt();
+        } catch (InputMismatchException i) {//i variavel local
+            System.out.println("Erro: " + i.getMessage() + "\nTente Novamente!");
+        }
+        return 99;
+    }// fim leiaNumInt
+
+    public static void menuPrincipal() {
+        System.out.println("        Menu Principal      ");
+        System.out.println("1 - Ger. Pessoa");
+        System.out.println("2 - Ger. Carro");
+        System.out.println("0 - Sair");
+        System.out.print("Digite aqui: ");
+    }// fim menuPricipal
+
+    public static void subMenu(int opM) {
+        String subM = null;
+        if (opM == 1) {
+            subM = "Pessoa";
+        }
+        if (opM == 2) {
+            subM = "Carro";
+        }
+        
+        System.out.println("Ger. " + subM + "   ");
+        System.out.println("1 - Cadastrar " + subM);
+        System.out.println("2 - Editar " + subM);
+        System.out.println("3 - Listar " + subM + "s");
+        System.out.println("4 - Deletar" + subM);
+        System.out.println("0 - Voltar");
+        System.out.println("Digite a opção: ");
+        
+
+    }// fim subMenu
+}
